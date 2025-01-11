8575e49c54f55cb3d15ac6f770d669fd915c283e
Indu Shekhar Jha <126399927+indu-shekhar@users.noreply.github.com>
2025-01-11 13:59:39 +0000

    update app.cpython-312.pyc, app.py and templates/index.html

--- app.py
+++ app.py
@@ -3,10 +3,8 @@ from cs50 import SQL
 from flask import Flask, redirect, render_template, request, session
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
-from helper import apology , login_required 
-
-
-
+from helper import apology, login_required
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
@@ -24,7 +22,8 @@ def after_request(response):
     response.headers["Pragma"] = "no-cache"
     return response
 
-
+# # Configure CS50 Library to use SQLite database
+# db = SQL("sqlite:///app.db")
 
 @app.route('/')
 # @login_required
@@ -56,8 +55,7 @@ def home():
             "tags": ["financial", "pension"]
         }
     ]
-    return render_template('index.html',schemes=schemes)
-
+    return render_template('index.html', schemes=schemes)
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -72,7 +70,7 @@ def login():
         if not request.form.get("username"):
             return apology("must provide username", 400)
 
-        # Ensure password was submitted21``
+        # Ensure password was submitted
         elif not request.form.get("password"):
             return apology("must provide password", 400)
 
@@ -97,8 +95,76 @@ def login():
     # User reached route via GET (as by clicking a link or via redirect)
     else:
         return render_template("login.html")
-    
 
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register user"""
+    if request.method == "POST":
+        # Get form data
+        first_name = request.form.get("firstName")
+        middle_name = request.form.get("middleName")
+        last_name = request.form.get("lastName")
+        age = request.form.get("age")
+        dob = request.form.get("dob")
+        gender = request.form.get("gender")
+        marital_status = request.form.get("maritalStatus")
+        phone_number = request.form.get("phoneNumber")
+        village_city = request.form.get("villageCity")
+        district = request.form.get("district")
+        state = request.form.get("state")
+        country = request.form.get("country")
+        pincode = request.form.get("pincode")
+        partner_name = request.form.get("partnerName")
+        parent_names = request.form.get("parentNames")
+        children_details = request.form.get("childrenDetails")
+        aadhar_number = request.form.get("aadharNumber")
+        aadhar_copy = request.files["aadharCopy"]
+        password = request.form.get("password")
+        confirm_password = request.form.get("confirmPassword")
+
+        # Validate form data
+        if not first_name or not age or not dob or not gender or not marital_status or not phone_number or not village_city or not district or not state or not country or not pincode or not aadhar_number or not aadhar_copy or not password or not confirm_password:
+            return apology("must provide all required fields", 400)
+
+        if password != confirm_password:
+            return apology("passwords do not match", 400)
+
+        if len(aadhar_number) != 12:
+            return apology("aadhar number must be exactly 12 digits", 400)
+
+        # Save aadhar copy
+        aadhar_copy_filename = secure_filename(aadhar_copy.filename)
+        aadhar_copy.save(os.path.join("uploads", aadhar_copy_filename))
+
+        # Hash password
+        hash = generate_password_hash(password)
+
+        # Insert user into database
+        print("First Name:", first_name)
+        print("Middle Name:", middle_name)
+        print("Last Name:", last_name)
+        print("Age:", age)
+        print("DOB:", dob)
+        print("Gender:", gender)
+        print("Marital Status:", marital_status)
+        print("Phone Number:", phone_number)
+        print("Village/City:", village_city)
+        print("District:", district)
+        print("State:", state)
+        print("Country:", country)
+        print("Pincode:", pincode)
+        print("Partner Name:", partner_name)
+        print("Parent Names:", parent_names)
+        print("Children Details:", children_details)
+        print("Aadhar Number:", aadhar_number)
+        print("Aadhar Copy Filename:", aadhar_copy_filename)
+        print("Password Hash:", hash)
+
+        # Redirect to login page
+        return redirect("/login")
+
+    else:
+        return render_template("registration.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
